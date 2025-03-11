67705fcbb0426a1261aeda77038705df951bd943
boyavamsi123 <vamvamshi56@gmail.com>
2025-03-11 09:33:48 +0530

    t

---
+++ Exception Handling/part-1/program4.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+
+public class program4{
+    //stacktrace
+    /*
+    * Exception name -  java.lang.ArithmeticException
+    * Exception message -  / by zero
+    * Which line number - program2.java:15
+    * Methods info - 
+    */
+    public static void main(String[] args){ 
+        method2();
+    }
+        public static void method2(){
+            method1();
+    }    
+        public static void method1(){
+        System.out.println("program execution start");
+        int fnumber = 9;
+        int snumber = 0;
+        int result = 0;
+
+        try {
+             result = fnumber / snumber;
+        } catch (ArithmeticException ae) {
+            System.out.println(Arrays.toString(ae.getStackTrace())); 
+        }
+        finally{
+            System.out.println("Finally Block");
+        }
+
+        System.out.println("out put :"+result);
+        System.out.println("program excution end");
+    }
+} 
