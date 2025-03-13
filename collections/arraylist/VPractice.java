8486b7e482207eb41c0607850f20321c411c7f75
boyavamsi123 <vamvamshi56@gmail.com>
2025-03-13 12:32:25 +0530

    t

---
+++ collections/arraylist/VPractice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class VPractice{
+    public static void main(String[] args) {
+        Vector v1 = new Vector();
+        v1.add("Yadigiri");
+        v1.add("Jeevan");
+        
+
+        System.out.println(v1); 
+    }
+}
